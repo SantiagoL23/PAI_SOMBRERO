ab8a34a9a40be38da893da572bcc3f764bc85950
TUPAKU <75865039+SantiagoL23@users.noreply.github.com>
2024-11-10 17:38:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// var posicionSombrero = 0;
-// var velocidad = 15; 
-// var intervaloMovimiento;
-
-// function iniciarMovimiento() {
-//     var sombrero = document.getElementById('sombrero');
-//     var contenedorJuego = document.querySelector('.contenedor-juego');
-
-//     intervaloMovimiento = setInterval(() => {
-//         posicionSombrero += velocidad;
-//         sombrero.style.left = posicionSombrero + 'px';
-
-//         if (posicionSombrero > contenedorJuego.offsetWidth) {
-//             posicionSombrero = -sombrero.offsetWidth;
-//         } }, 23);
-// }
-
-// function detenerMovimiento() {
-//     clearInterval(intervaloMovimiento);
-// }
-
-// window.onload = iniciarMovimiento;
-
-// document.getElementById('sombrero').onclick = detenerMovimiento;
-
-
 const personaje = document.getElementById('personaje');
 const sombrero = document.getElementById('sombrero');
 const puntajeMostrar = document.getElementById('puntos');
