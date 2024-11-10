6fbf2e6822ef4cd99e85cfedb2ecaa6c8b3364d7
TUPAKU <75865039+SantiagoL23@users.noreply.github.com>
2024-11-10 17:53:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+// var posicionSombrero = 0;
+// var velocidad = 15; 
+// var intervaloMovimiento;
+
+// function iniciarMovimiento() {
+//     var sombrero = document.getElementById('sombrero');
+//     var contenedorJuego = document.querySelector('.contenedor-juego');
+
+//     intervaloMovimiento = setInterval(() => {
+//         posicionSombrero += velocidad;
+//         sombrero.style.left = posicionSombrero + 'px';
+
+//         if (posicionSombrero > contenedorJuego.offsetWidth) {
+//             posicionSombrero = -sombrero.offsetWidth;
+//         } }, 23);
+// }
+
+// function detenerMovimiento() {
+//     clearInterval(intervaloMovimiento);
+// }
+
+// window.onload = iniciarMovimiento;
+
+// document.getElementById('sombrero').onclick = detenerMovimiento;
+
+
 const personaje = document.getElementById('personaje');
 const sombrero = document.getElementById('sombrero');
 const puntajeMostrar = document.getElementById('puntos');
+let velocidadSombrero = 3;
 
 let puntaje = 0;
 let sombreroDetenido = false; // para saber si el gorro esta detenido
@@ -23,6 +50,7 @@ function pararSombrero() {
     if (distanciaHorizontal < 8) { // Asegura una tolerancia de 15px para el acierto
         puntaje++;
         puntajeMostrar.textContent = puntaje;
+        velocidadSombrero *= 0.9; // Aumenta la velocidad reduciendo el tiempo
     }
 
     // Reiniciar el sombrero después de 1 segundo
@@ -30,7 +58,7 @@ function pararSombrero() {
         sombreroDetenido = false;
         sombrero.style.animation = 'none'; // Reinicia la animación
         void sombrero.offsetWidth; // Forzar el reinicio
-        sombrero.style.animation = 'moveHorizontal 2s linear infinite'; // Reinicia el movimiento
+        sombrero.style.animation = `moveHorizontal ${velocidadSombrero}s linear infinite` // Reinicia el movimiento
     }, 500);
 }
 
